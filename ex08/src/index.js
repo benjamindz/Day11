f862d803c40cb6a0480e6c827202459da123e42d
benjamindz <dzaferovicbenjamin@gmail.com>
2021-03-19 01:28:53 +0100

    compared letters in words

---
+++ ex08/src/index.js
@@ -0,0 +1,24 @@
+function myMutation(arr){
+    var firstWord = arr[0];
+    var secondWord = arr[1];
+
+    for (i = 0; i < secondWord.length; i++) {
+        if(firstWord.toLowerCase().includes(secondWord[i].toLowerCase()) == false)
+        return false;
+    }
+    return true;
+}
+
+console.log(myMutation(["hello", "hey"]));
+console.log(myMutation(["hello", "Hello"]));
+console.log(myMutation(["zyxwvutsrqponmlkjihgfedcba", "qrstu"]));
+console.log(myMutation(["Mary", "Army"]));
+console.log(myMutation(["Mary", "Aarmy"]));
+console.log(myMutation(["Alien", "line"]));
+console.log(myMutation(["floor", "for"]));
+console.log(myMutation(["hello", "neo"]));
+console.log(myMutation(["voodoo", "no"]));
+console.log(myMutation(["ate", "date"]));
+console.log(myMutation(["Tiger", "Zebra"]));
+console.log(myMutation(["Noel", "ole"]));
+module.exports = myMutation;
