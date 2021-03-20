9e5c3a22dd795894399865ce3a4725262896797d
benjamindz <dzaferovicbenjamin@gmail.com>
2021-03-20 23:16:48 +0100

    Updated this excercise

--- ex03/src/index.js
+++ ex03/src/index.js
@@ -3,10 +3,8 @@ myPetsArray = ["Dog", "Cat"];
 function myArrayFunction(myPets) {
   var myNewPets = [...myPets];
   myNewPets.push("Bird", "Fish");
-  var firstPet = myNewPets[0];
-  var lastPet = myNewPets[myNewPets.length - 1];
-  myNewPets.pop();
-  myNewPets.shift();
+  var firstPet = myNewPets.shift();
+  var lastPet = myNewPets.pop();
   myNewPets.unshift("Lion");
 
   return myNewPets;
